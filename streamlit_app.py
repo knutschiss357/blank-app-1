becd250a15e9c7eb8c988b7be797fb2126330576
knutschiss357 <knutschiss357@gmail.com>
2025-10-24 16:21:10 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,11 +10,3 @@ Kia ora! My name is Chisato.
 I’m a Japanese Language Adviser based in New Zealand, supporting teachers and promoting Japanese language education.  
 I’m passionate about language learning, cultural exchange, and building connections through communication.
 """)
-
-# 画像を追加（同じフォルダに "profile.jpg" を置く場合）
-st.image("profile.jpg", caption="こんにちは！Chisatoです", width=300)
-
-# 追加のメッセージ
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
