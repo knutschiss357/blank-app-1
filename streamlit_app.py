afc3f1db49e098e34c4449c0dae08e2d46f8655d
knutschiss357 <knutschiss357@gmail.com>
2025-10-24 16:28:18 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.title("🎈 My new app")
 # 自己紹介セクション
 st.header("About Me")
 st.write("""
-最初の第一歩
+最初の第一歩  
 Kia ora! My name is Chisato.  
 I’m a Japanese Language Adviser based in New Zealand, supporting teachers and promoting Japanese language education.  
 I’m passionate about language learning, cultural exchange, and building connections through communication.
